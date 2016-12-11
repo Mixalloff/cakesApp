03bd224d85101cc59ef70128c6ad241d13782c9d
Mikhail Mikhalev <Mikhail Mikhalev>
2016-12-11 14:14:30 +0300

    Изменены стили приложения, внесены правки в меню и навбар

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+const profile = {
+  name: 'Гагарина Александра',
+  email: 'aleksandra.gagarina@gmail.com',
+  image: '../../assets/images/main-logo.jpg',
+  background: './../assets/images/background.jpg'
+}
+
 @Component({
   selector: 'ck-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
+  profile = profile;
 
   constructor() { }
 
