e9a23b790b6886a759cc6ff926c5a784e0205742
Mikhail Mikhalev <Mikhail Mikhalev>
2016-12-11 20:59:04 +0300

    Починен materialize-css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import "materialize-css";
 import { MaterializeModule } from "angular2-materialize";
 import { appRoutes } from './app.routes';
 
